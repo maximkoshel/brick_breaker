3e8fe572a299a23a5f9c5d1b66cbc6e2df04501a
maximkoshel <maximkoshel95@gmail.com>
2019-10-26 19:19:33 +0300

    Addaed brick phisics and ball draw

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 import pygame
 
+#Player Class-------------------------------------------------------------------
 class player():
     def __init__(self,x,y,height,width) :
         self.x=x
         self.y=y
         self.height = height
         self.width = width
-        self.velocity = 10
+        self.velocity = 15
 
     def draw(self,WIN):
         BLUE=(0,0,255)
@@ -15,13 +16,27 @@ class player():
         if(self.x>=850):
             self.x=850
         pygame.draw.rect(WIN,BLUE,(self.x,self.y,150,20))
-        pygame.display.update()
+#-------------------------------------------------------------------------------
 
+#Ball class---------------------------------------------------------------------
+class ball():
+    def __init__(self,x,y) :
+        self.x=x
+        self.y=y
+        self.velocity = 30
+
+    def draw(self,WIN):
+        BLUE=(0,255,0)
+        if(self.x<=0):
+            self.x=0
+        if(self.x>=850):
+            self.x=850
+        pygame.draw.circle(WIN, BLUE,(self.x,self.y),10)
+#-------------------------------------------------------------------------------
 def updateGame():
     WIN.blit(BACKGROUND,(0,0))
     brick.draw(WIN)
-
-
+    ball.draw(WIN)
 
 #Main---------------------------------------------------------------------------
 pygame.init()
@@ -30,12 +45,14 @@ pygame.display.set_caption("Brick Breaker")
 BACKGROUND = pygame.image.load("background.jpg")
 
 brick = player(425,580,150,20)
+ball = ball(50,50)
 
 run=True
 CLOCK =pygame.time.Clock()
 while run :
     CLOCK.tick(30)
     updateGame()
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] :
         brick.x -= brick.velocity
@@ -45,5 +62,4 @@ while run :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
 #Main---------------------------------------------------------------------------
